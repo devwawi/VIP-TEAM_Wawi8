74c63147a358de9b0543e7e5dc99033c6cad5dff
devwawi <coketigoju@cartelera.org>
2016-09-18 03:53:12 +0300

    Add files via upload

---
+++ plugins/VIRSON.lua
@@ -0,0 +1,34 @@
+do
+
+function run(msg, matches)
+  return [[ 
+اهــلا بـكـم فـي سـورس 
+                                                    🎗VIP🏅TEAM🎗
+
+🎗الاصـدار🏅الانـكـلـيـزي {2}🔹
+
+🎗للتحدث مع المطورين راسل احدهم👇🏻
+
+🎗 1. @XxX_TEAM_XxX 
+🎗2. @i_d_b 
+🎗3. @lIlDevlIl
+🎗4. @A_6_Q
+🎗5. @mustafa_dev
+▫️➖▫️➖▫️➖▫️➖▫️➖▫️
+
+🎗تـابـع قـنـاة الـسـورس لـتطـويـر بـوتـك 🏅👇🏻
+
+https://telegram.me/VIP_TEAM1
+]]
+end
+
+return {
+  description = "", 
+  usage = "!السورس:",
+  patterns = {
+    "^السورس$"
+  }, 
+  run = run 
+}
+
+end
